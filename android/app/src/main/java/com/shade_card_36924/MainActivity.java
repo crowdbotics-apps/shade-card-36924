b3c4a2889452f34bc470707cf4faf67dedac2b1f
Crowdbotics <team@crowdbotics.com>
2022-08-26 12:27:28 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/shade_card_36924/MainActivity.java
@@ -0,0 +1,15 @@
+package com.shade_card_36924;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "shade_card_36924";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "shade_card_36924",
+  url: "https://shade_card_36924.botics.co",
+  api: "https://shade_card_36924.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
